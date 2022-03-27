d8bef08e02649b9c19251974030bdbd7b8da4090
Vaishali <Vaishali@DESKTOP-MCAI5MI>
2022-03-27 21:57:34 +0530

    app user

--- Project2/src/com/hibernate/App.java
+++ Project2/src/com/hibernate/App.java
@@ -2,32 +2,35 @@ package com.hibernate;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
-import com.hibernate.Project2.users;
-import com.hibernate.Project2.users.user;
+
+import com.hibernate.Project2.Users1;
+
 public class App {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		SessionFactory factory = new Configuration()
                 .configure("hibernate1.cfg.xml")
-                .addAnnotatedClass(users.class)
+                .addAnnotatedClass(Users1.class)
                 .buildSessionFactory();
 
 Session session = factory.getCurrentSession();
 
 try {
 // Create object of entity class type
-	users user=new users();
-//users user = new users( 1, "vaishu","purushothaman","hah@tns.org");
+	Users1 us1=new Users1();
+//Users1 us1= new Users1( 2, "y","purushothaman","vbnh@tns.org");
 // Start transaction
 session.beginTransaction();
-user=session.get(users.class, 1);
-//user.setUsers_fname=("yamini");
-session.delete(user);
+us1=session.get(Users1.class,0);
+//updating object
+us1.setUsers1_fname=("yamini");
+session.delete(us1);
 // Perform operation
-session.save(user);
+//session.save(us1);
 // Commit the transaction 
 session.getTransaction().commit();
-System.out.println("user");
+//System.out.println("row added");
+System.out.println(us1);
 } finally {
 session.close();
 factory.close();
